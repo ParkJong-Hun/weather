e1e5862526249c10d4a6b25b91eea2bdc7c30daf
Park JongHun <thesn515@gmail.com>
2024-05-16 21:48:56 +0900

    feat: coroutine scope

--- composeApp/src/commonMain/kotlin/infrastructure/repository/ExampleRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/infrastructure/repository/ExampleRepositoryImpl.kt
@@ -2,11 +2,16 @@ package infrastructure.repository
 
 import domain.gateway.repository.ExampleRepository
 import infrastructure.api.ExampleApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
 
 class ExampleRepositoryImpl(
     private val exampleApi: ExampleApi,
 ) : ExampleRepository {
     override suspend fun getPerson(): String {
-        return exampleApi.getPerson()
+        return withContext(Dispatchers.IO) {
+            exampleApi.getPerson()
+        }
     }
 }
