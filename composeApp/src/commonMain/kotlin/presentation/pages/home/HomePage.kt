b4f9234da9fdd38bb74e25c047d5c469e161b4fa
Park JongHun <thesn515@gmail.com>
2024-05-24 09:26:15 +0900

    feat: prototyping home page

--- composeApp/src/commonMain/kotlin/presentation/pages/home/HomePage.kt
+++ composeApp/src/commonMain/kotlin/presentation/pages/home/HomePage.kt
@@ -1,21 +1,33 @@
 package presentation.pages.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavGraphBuilder
+import compose_multiplatform_test_airfield.composeapp.generated.resources.Res
+import compose_multiplatform_test_airfield.composeapp.generated.resources.ic_map_24
+import domain.entity.City
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import presentation.di.koinViewModel
+import presentation.pages.home.components.AdditionalInfoText
 
 // TODO use SharedTransitionScope
 @Composable
@@ -25,39 +37,71 @@ fun NavGraphBuilder.HomePage(
     val state by homeViewModel.uiState.collectAsState()
     HomePageBody(
         state = state,
-        onClickButton = (homeViewModel::onClickExampleButton)
+        onClickSetting = (homeViewModel::onClickCity)
     )
 }
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 private fun HomePageBody(
     state: HomeUiState,
-    onClickButton: () -> Unit,
+    onClickSetting: (City) -> Unit,
 ) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Row {
+            Text(
+                text = state.title,
+                style = TextStyle(
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 24.sp,
+                    textMotion = TextMotion.Animated,
+                ),
+                maxLines = 1,
+            )
+            Icon(
+                painter = painterResource(Res.drawable.ic_map_24),
+                contentDescription = "Location",
+                tint = MaterialTheme.colors.primary,
+                // FIXME : Navigate to the search page
+                modifier = Modifier.clickable {
+                    onClickSetting(City.Osaka)
+                }
+            )
+        }
+        Spacer(
+            modifier = Modifier.height(16.dp),
+        )
         Text(
-            fontSize = 36.sp,
-            text = state.person,
+            text = state.temperature ?: "",
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 36.sp,
+            ),
+            maxLines = 1,
         )
         Spacer(
-            modifier = Modifier.height(16.dp)
+            modifier = Modifier.height(16.dp),
         )
-        Button(
-            onClick = onClickButton,
-        ) {
-            Text("Get Person")
-        }
+        AdditionalInfoText(
+            title = "Humidity:",
+            description = state.humidity ?: "",
+        )
+        AdditionalInfoText(
+            title = "Rainfall:",
+            description = state.rainfall ?: "",
+        )
     }
 }
 
 @Preview
 @Composable
+@Suppress("unused")
 private fun HomePageBodyPreview() {
     HomePageBody(
         state = HomeUiState(),
-        onClickButton = {},
+        onClickSetting = {},
     )
 }
