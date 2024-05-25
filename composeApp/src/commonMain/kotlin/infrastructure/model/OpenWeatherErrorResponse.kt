abb252b2293af3ec10f04a063f6a6c0fb7470e59
Park JongHun <thesn515@gmail.com>
2024-05-25 17:08:37 +0900

    feat: create error response

---
+++ composeApp/src/commonMain/kotlin/infrastructure/model/OpenWeatherErrorResponse.kt
@@ -0,0 +1,12 @@
+package infrastructure.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class OpenWeatherErrorResponse(
+    @SerialName("cod")
+    val errorCode: Int,
+    @SerialName("message")
+    val errorMessage: String,
+)
